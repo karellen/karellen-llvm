1c57dcfb997a0c1538e2f5bd3cb98ed7ca4f1d64
Arcadiy Ivanov <arcadiy@ivanov.biz>
2023-10-23 04:26:55 -0400

    Reset .release [skip ci]
    Fix package-twostage.sh packager invocation

--- package-twostage.sh
+++ package-twostage.sh
@@ -4,4 +4,4 @@ SOURCE_DIR="$(readlink -nf llvm-project/llvm)"
 BUILD_DIR="$(readlink -nf llvm.twostage.build)"
 TARGET_DIR="$(readlink -nf llvm.twostage.bin)"
 
-./packager.py -b "$BUILD_DIR/tools/clang/stage2-bins" -t "$TARGET_DIR" -s "$SOURCE_DIR"
+./packager.py -S 2 -b "$BUILD_DIR" -t "$TARGET_DIR" -s "$SOURCE_DIR"
